16ae8a2eb754fb145ccfea68d22a80c51a394dcd
João Victor <va60234@gmail.com>
2026-03-20 19:18:32 -0300

    Implement number range check between 1 and 10

--- Condicionais/VerificarIntervaldo1e10.java
+++ Condicionais/VerificarIntervaldo1e10.java
@@ -2,23 +2,17 @@ import java.util.Scanner;
 
 public class VerificarIntervaldo1e10 {
     public static void main(String[] args) {
-        Scanner scan = Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        
+        System.out.println("Digite um número: ");
+        int num = scan.nextInt();
+
+        if (num >= 0 && num <= 10) {
+            System.out.println("O valor está entre 1 e 10");
+        } else {
+            System.out.println("O valor não está entre 1 e 10");
+        }
+
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 }
